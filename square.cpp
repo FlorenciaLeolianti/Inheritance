f47fd7ade608ee1aed70feda9066e7726187a4d1
Evans Jahja <evans.jahja@yahoo.com>
2015-06-20 14:42:58 -0700

    Class Square

---
+++ square.cpp
@@ -0,0 +1,10 @@
+#include "square.h"
+Square::Square()
+{
+	Square(1.0);
+}
+Square::Square(float sisi)
+{
+	panjang=sisi;
+	lebar=sisi;
+}
---
+++ square.h
@@ -0,0 +1,9 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+#include "rectangle.h"
+class Square : Rectangle
+{
+	Square();
+	Square(float sisi);
+}
+#endif
