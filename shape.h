0ab132cb1284be177b8b527cc7c616cd24e36eae
FlorenciaLeolianti <little_flo@ymail.com>
2015-06-20 15:03:33 +0700

    getluas getkeliling

--- shape.h
+++ shape.h
@@ -7,6 +7,8 @@ using namespace std;
 class Shape
 {
 	public:
+		float getLuas();
+		float getKeliling();
 		virtual void hitungLuas()=0; //pure virtual function
 		virtual void hitungKeliling()=0;
 	protected:
